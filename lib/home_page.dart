11454c16ffb49e4ec41d17f246c03fcfe7d2437b
Andre Lucas <alucas748@gmail.com>
2023-09-26 21:27:45 -0300

    Interface do aplicativo criada

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,12 +8,73 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  final TextEditingController _tituloController = TextEditingController();
+  final TextEditingController _descricaoController = TextEditingController();
+
+  _exibirTelaCadastro() {
+
+    showDialog(
+      context: context, 
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Adicionar anotação"),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: _tituloController,
+                autofocus: true,
+                decoration: const InputDecoration(
+                  labelText: "Título",
+                  hintText: "Digite o título"
+                ),
+              ),
+              TextField(
+                controller: _descricaoController,
+                decoration: const InputDecoration(
+                  labelText: "Descrição",
+                  hintText: "Digite a descrição"
+                ),
+              )
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context), 
+              child: const Text("Cancelar")
+            ),
+            TextButton(
+              onPressed: () {
+
+                //Salvar
+
+                Navigator.pop(context);
+              }, 
+              child: const Text("Salvar")
+            )
+          ],
+        );
+      }
+    );
+
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Anotações"),
+        title: const Text("Minhas anotações"),
         backgroundColor: Colors.green,
+      ),
+      body: Container(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _exibirTelaCadastro();
+        },
+        backgroundColor: Colors.green,
+        foregroundColor: Colors.white,
+        child: const Icon(Icons.add),
       ),
     );
   }
